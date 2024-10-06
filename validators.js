604c3d6db8cac8f88628a94709ebecf1409f5232
MavNode <168236731+MavNode@users.noreply.github.com>
2024-10-06 17:55:18 +0200

    Create validators.js

---
+++ validators.js
@@ -0,0 +1,202 @@
+const validators = {
+    "shidovaloper1xpp7uxrfpsz40uc27yxg527fc0uhznmnxn6hl0": {
+        "base64": "4gsuHfWgfm+ZesKkaknSsXZCfe0=",
+            "valcons": "shidovalcons1ug9ju8045plxlxt6c2jx5jwjk9myyl0dplv3zx"
+                // "name": "Shido Six"
+    },
+    "shidovaloper1jxyn0cata5n74py4lgyuzvyvjwmhfxq3heds28": {
+        "base64": "LVYeOjPG19RUj7KpU95x6uPuRnY=",
+            "valcons": "shidovalcons194tpuw3ncmtag4y0k2548hn3at37u3nkq8qljj"
+                // "name": "Superbit"
+    },
+    "shidovaloper13at9nycw48ll8d4wauau2tfsl3vj9g879v28rn": {
+        "base64": "Z61qbE8tK+r+OysxOTE4AqE9XoA=",
+            "valcons": "shidovalcons1v7kk5mz095474l3m9vcnjvfcq2sn6h5qdhpc3g"
+                // "name": "Shido Observer"
+    },
+    "shidovaloper1klykygqtw4v0nwleznqqn92hv5f52fe282fw70": {
+        "base64": "5dq1B/Z03lNqehSRAZ69RuyDbYA=",
+            "valcons": "shidovalcons1uhdt2plkwn09x6n6zjgsr84agmkgxmvq2jdkdn"
+                // "name": "Maverick | MavNode"
+    },
+    "shidovaloper1nq2sk5rlzunju00uv5nhagpw48awwrnqmd504n": {
+        "base64": "FoDfIjyuOhFI4O/O14U1a8L7dHQ=",
+            "valcons": "shidovalcons1z6qd7g3u4capzj8qal8d0pf4d0p0kar52cs9t0"
+                // "name": "256x25"
+    },
+    "shidovaloper1529m29q7kfkvnps29fw60qng9kawstzam84dzu": {
+        "base64": "qIz8G/txZWxHSRsV0FcX1aa38nk=",
+            "valcons": "shidovalcons14zx0cxlmw9jkc36frv2aq4ch6knt0uneavqx58"
+                // "name": "Kensei"
+    },
+    "shidovaloper1v7xd0h8d6l92epq9x28g2c4haxh5n059ct2vkn": {
+        "base64": "3thzxXaWXeIhKUM1JL3mUfYgmDo=",
+            "valcons": "shidovalcons1mmv883tkjew7ygffgv6jf00x28mzpxp6csydx5"
+                // "name": "Shido Seven"
+    },
+    "shidovaloper1wftglqnx7c25sqnvp48f7tr426e2zvrcxs88qr": {
+        "base64": "mub0bytogzIrJMANSmjXErt1qKE=",
+            "valcons": "shidovalcons1ntn0gmetdzpny2eycqx556xhz2aht29pdx4f6l"
+                // "name": "NeoNode"
+    },
+    "shidovaloper1l5wrzzst5w07t4fj97kslkumtufwtzqc4c2mg6": {
+        "base64": "CPAwTVZ37+9B9l8nzp1Az7zWHVA=",
+            "valcons": "shidovalcons1prcrqn2kwlh77s0ktunua82qe77dv82slc74he"
+                // "name": "Nuke Node"
+    },
+    "shidovaloper1523cazt7daktat098pke99zzfguz89ch6ja5g9": {
+        "base64": "M2W9ixqw4NCIHK295hj+WlLwJVc=",
+            "valcons": "shidovalcons1xdjmmzc6krsdpzqu4k77vx87tff0qf2htn4lte"
+                // "name": "Franklinvesting"
+    },
+    "shidovaloper1xtz9vsuugv8xfruwnqrpw8fg9lw8js4ayvpvzy": {
+        "base64": "qS2b1HVb3wz7dhi1jt72C+oGoY4=",
+            "valcons": "shidovalcons14ykeh4r4t00se7mkrz6cahhkp04qdgvwx6z9h2"
+                // "name": "Shido Two"
+    },
+    "shidovaloper1p9dm3l7d05lnfmrggggqv2dxrzr2xjfevh32aq": {
+        "base64": "ijgfNdvelLxTzNAIPz7HCS3yKCU=",
+            "valcons": "shidovalcons13gup7dwmm62tc57v6qyr70k8pykly2p9r723ek"
+                // "name": "WhenMoon"
+    },
+    "shidovaloper15ullpqu8yu23cqjcvxycf4n409zdnvrxv2fgal": {
+        "base64": "tcTSMA1GsazrSPcDYcU+lbSWE9Q=",
+            "valcons": "shidovalcons1khzdyvqdg6c6e66g7upkr3f7jk6fvy75qdeajw"
+                // "name": "Fox Node"
+    },
+    "shidovaloper1z35m6f47hjpfvd30naxpvwk5f0hgpc3krf5ezj": {
+        "base64": "zQR4GIZ82cCP9G6jjpOT8J9EjcA=",
+            "valcons": "shidovalcons1e5z8sxyx0nvuprl5d63cayun7z05frwqxfua0k"
+                // "name": "DefiMike"
+    },
+    "shidovaloper1k59k9x8e5k7wqhq86x2ntq4r650mdruav3nj5k": {
+        "base64": "VQ2t2YB7nYutKpgEbHTAJZpXWEY=",
+            "valcons": "shidovalcons125x6mkvq0wwchtf2nqzxcaxqykd9wkzxpegspr"
+                // "name": "ShidoForge"
+    },
+    "shidovaloper1s93mcr0823njekzu4husd9tejfl0l6w2hy43dt": {
+        "base64": "Ea1R2919Bk5bY56Jr7wIFx1X2ZQ=",
+            "valcons": "shidovalcons1zxk4rk7a05ryukmrn6y6l0qgzuw40kv58c2wtx"
+                // "name": "ShidoGuard"
+    },
+    "shidovaloper1gdg92dfn5up40lgfwx9rxa2ed7r2wvvkx3jgyc": {
+        "base64": "45e98F28BKYnGbgg1QvFXdmumik=",
+            "valcons": "shidovalcons1uwtmmuzahsz2vfcehqsd2z79thv6ax3f9654mn"
+                // "name": "MagicShido"
+    },
+    "shidovaloper162zcltlsn868klgdmtxr53dun3as5pfqz046vs": {
+        "base64": "ug3UkCxaZffgDZd72aFJkKZPZo4=",
+            "valcons": "shidovalcons1hgxafypvtfjl0cqdjaaang2fjzny7e5wcldhq3"
+                // "name": "CryptoWav3z"
+    },
+    "shidovaloper18w4lpkcurhvpetdundnn0ke0cwggunrzc6xghy": {
+        "base64": "tdpIgRKgeXOucDrMnwwmS/5DIvk=",
+            "valcons": "shidovalcons1khdy3qgj5puh8tns8txf7rpxf0lyxghegxk2fc"
+                // "name": "MetaDefi"
+    },
+    "shidovaloper1l22s8pngtada0fuddlttszrakr2q9rhskr6ceh": {
+        "base64": "I9HiHx0maX5wqMngdsuWLd4DdrA=",
+            "valcons": "shidovalcons1y0g7y8caye5huu9ge8s8djuk9h0qxa4srg054q"
+                // "name": "Blue Trust"
+    },
+    "shidovaloper1q43twwh2mnej3p3ffnrgc9fq2vh5p6hhc2w7pa": {
+        "base64": "wKQXQfnCs3YJyIooqEKt1B/g6ow=",
+            "valcons": "shidovalcons1czjpws0ec2ehvzwg3g52ss4d6s07p65vzn27n0"
+                // "name": "Olim"
+    },
+    "shidovaloper1gja7pjqh8ltupejuu5rhm57cvjgenr3v0wvz40": {
+        "base64": "ZzbyGLqGrbPlYxdkDYE6ZadqOYY=",
+            "valcons": "shidovalcons1vum0yx96s6km8etrzajqmqf6vknk5wvxtzw0q0"
+                // "name": "Shido Four"
+    },
+    "shidovaloper1h4rytdqlfgwazj9g7px9f802yavg2m3f6gzswv": {
+        "base64": "Mliuz2GXm/u/WBPGgsjKnB03sp0=",
+            "valcons": "shidovalcons1xfv2anmpj7dlh06cz0rg9jx2nswn0v5a24rprm"
+                // "name": "ShidoFrance"
+    },
+    "shidovaloper1gsmhz20fteu5hyy4d29hn07ly6lstgzcg0q7rz": {
+        "base64": "7/WPrUVZMZIzwoL5QsOKC+j1ASA=",
+            "valcons": "shidovalcons1al6clt29tyceyv7zstu59su2p0502qfq4erl9a"
+                // "name": "Scafire Node"
+    },
+    "shidovaloper1u85q723mxh0spxft44m2w0hjw65fuy8wu2du20": {
+        "base64": "muib9ZYfQr6JCU86yUNx00PzyhA=",
+            "valcons": "shidovalcons1nt5fhavkraptazgffuavjsm36dpl8jssmt8am7"
+                // "name": "Shido Five"
+    },
+    "shidovaloper14pxfd0uwz9kduu2jpqelmgu9hrpg9xw5ddseh3": {
+        "base64": "9WEzWWiBu51GIw7SzRgk+FuDvBU=",
+            "valcons": "shidovalcons174snxktgsxae633rpmfv6xpylpdc80q4eecf0l"
+                // "name": "Shido Three"
+    },
+    "shidovaloper1utzuagl5zgpq67vqn09qladevgm79zj7uxapu7": {
+        "base64": "uQH1KBYWEYKA0EZVchZnAbY39sA=",
+            "valcons": "shidovalcons1hyql22qkzcgc9qxsge2hy9n8qxmr0akqkhceus"
+                // "name": "ShidoVerse"
+    },
+    "shidovaloper190qfkmdc2nmpuacdlp4wnqu00949h87h674w86": {
+        "base64": "l/z+NM2h/K2fhnmYz3Op8IRAp1E=",
+            "valcons": "shidovalcons1jl70udxd5872m8ux0xvv7uaf7zzypf63f42rvf"
+                // "name": "ShidoDutch"
+    },
+    "shidovaloper190jcx22hqnhyarpagefpl3lu0gcz5jj3nm4tp2": {
+        "base64": "RdBpFZicWiS8TUJoIHBtm4AxrmE=",
+            "valcons": "shidovalcons1ghgxj9vcn3dzf0zdgf5zqurdnwqrrtnpjut8tm"
+                // "name": "ShidoAustralia"
+    },
+    "shidovaloper1q3lny268kqjq9urz5yy5xa4d7j3xyscr6wn8ff": {
+        "base64": "MLYvDvZnFKvorKe9AVy7ZQHYzgo=",
+            "valcons": "shidovalcons1xzmz7rhkvu22h69v577szh9mv5qa3ns2n5jq5l"
+                // "name": "Chaintools"
+    },
+    "shidovaloper1pa7j703548n9kd9fpptakdel3phgnx4enrqaeu": {
+        "base64": "QgGRVBWGJ26k12nBAQpXuzT4dKA=",
+            "valcons": "shidovalcons1ggqez4q4scnkafxhd8qszzjhhv60sa9q6dxn25"
+                // "name": "Shido4life"
+    },
+    "shidovaloper16fsn8mlm7e29854zdyuwz5vtp3teh0s940qclt": {
+        "base64": "lXLyUPlmqeJ8G8U0K5OcZBsMQEM=",
+            "valcons": "shidovalcons1j4e0y58ev657ylqmc56zhyuuvsdscszr9n58hd"
+                // "name": "In Bjorn we trust"
+    },
+    "shidovaloper145xez3t96wz9st98vhzrexjaumydvkyq0tp2gy": {
+        "base64": "pzuYCs+YH3/FDEQdC0N/4BJcGEY=",
+            "valcons": "shidovalcons15uaeszk0nq0hl3gvgswsksmluqf9cxzxwp5r6f"
+                // "name": "Indonode"
+    },
+    "shidovaloper137umxkw5gdvj2vw6y7c3nfa6j55wfsqyk44ze7": {
+        "base64": "jcGP+SD6BJ4BuR9LHj7dvsFT34I=",
+            "valcons": "shidovalcons13hqcl7fqlgzfuqdera93u0kahmq48huzu2me5j"
+                // "name": "Blockval"
+    },
+    "shidovaloper14xyamsv4xh3m2l7ytmgae4x66y2ggx3pvrdru7": {
+        "base64": "z43yuYIxAt4jVLkIeg4NTBqGHYQ=",
+            "valcons": "shidovalcons1e7xl9wvzxypdug65hyy85rsdfsdgv8vy6ndwvq"
+                // "name": "BG-SHI-VAL01"
+    },
+    "shidovaloper1he93kchedrt60jqs8mj9vvp5s47hklwy85v9uy": {
+        "base64": "LA6Okj+5JDukE3PmAxDiJFbHVPU=",
+            "valcons": "shidovalcons1qxjmnfjfq3q3yyxn6tdz7vkw7xj0k2pjmpte72"
+                // "name": "Neev Labs"
+    },
+    "shidovaloper1s385m6ua7rxl504eh39sxefu3qgtuzzyxrw6j5": {
+        "base64": "QMTmHvxMI6HWMsuCNsSkMfMgU4Q=",
+            "valcons": "shidovalcons1grzwv8hufs36r43jewprd39yx8ejq5uyr2w7y7"
+                // "name": "xshrimp"
+    },
+    "shidovaloper10f3amghrxx3xx2qzp74u5sgt4lyakp8tf87er9": {
+        "base64": "cuGHpY8yk2GHhSX1xM9HsebQ1Ck=",
+            "valcons": "shidovalcons1wtsc0fv0x2fkrpu9yh6ufn68k8ndp4pffeuqze"
+                // "name": "Carnival Consensus 🎪"
+    },
+    "shidovaloper16zh2lzdmrh9cmqc0x8qp87ta8cggvsvax7mc58": {
+        "base64": "5YmJgB6lTLg1cN8/+y44L+uyV3I=",
+            "valcons": "shidovalcons1ukycnqq754xtsdtsmullkt3c9l4my4mjyhf3s3"
+                // "name": "ShidoValidatorX"
+    },
+    "shidovaloper1enknwkxy5qsm8qjqj583h9z2h7gvnyfvfpjnwq": {
+        "base64": "G7nm0U4WAcgq0cJFLNXddwE8EKY=",
+            "valcons": "shidovalcons1rwu7d52wzcqus2k3cfzje4wawuqncy9xk005xf"
+                // "name": "Sherlock Holmes"
+    }
+};
